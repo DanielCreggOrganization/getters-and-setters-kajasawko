c9a15df277963400ec16216e915995526dd14a1d
kajasawko <kaja.sawko@gmail.com>
2020-02-27 14:37:21 +0000

    ...

--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        Person newPersonObj = new Person();
+
+        newPersonObj.setFirstName("Sarah");
+        newPersonObj.setMiddleNames("Jessica");
+        newPersonObj.setUserName("theRedRhino");
+
+        System.out.println(newPersonObj.getFirstName());
+        System.out.println(newPersonObj.getUserName());
+        
+    }
     // Main method
+
+  
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,40 @@
 class Person {
     // Instance Variables
-    
-    // Methods
+    private String firstName;
+    private String[] middleNames;
+    private String userName;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleNames(String middleNames) {
+        this.middleNames = middleNames.split(" ");
+        
+    }
+
+    public void setUserName(String userName) {
+        if(userName.length() > 10){
+            this.userName = userName.substring(0,10);
+        }else{
+            this.userName = userName;
+        }
+        
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void getMiddleNames() {
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < middleNames.length; i++){
+            sb.append(middleNames[i] + " ");
+        }
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
 }
